8f537f60c94da0fb5fca069d8eb8e3ced5ab9b51
sh4869 <nobuk4869@gmail.com>
2018-05-16 16:03:12 +0900

    重さ制限を考慮

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ void checkCost(Address address, ShortestPathMap pathmap) {
 
 int main(void) {
     std::cout << "------- 問1 -------" << std::endl;
-    
+
     auto address = readAddress("data/address.txt");
     auto map = RoadGraph(x, 5, 30);
     Dijkstra d = Dijkstra(x);
@@ -40,18 +40,18 @@ int main(void) {
     ShortestPathMap pathmap = ShortestPathMap();
     pathmap.AddAdress(address, d);
 
-    auto solver = Solver(pathmap, address, 40);
+    auto solver = Solver(pathmap, address, 40, 1000);
     std::cout << ">>>>> Greedy法(一度，決め打ち)" << std::endl;
-    PrintResult(solver.SolveGreedy(), address);
+    PrintResult(solver.SolveGreedy(1), address);
     std::cout << ">>>>> Greedy法(ランダム選択を複数回)" << std::endl;
-    PrintResult(solver.SolveGreedyRandom(), address);
+    PrintResult(solver.SolveGreedyRandom(1), address);
     std::cout << ">>>>> 焼きなまし法" << std::endl;
     PrintResult(solver.SA(), address);
     std::cout << ">>>>> Greedy法(複数台，決め打ち)" << std::endl;
-    solver.SolveGreedyMulti(2);
+    PrintResult(solver.SolveGreedy(2), address);
     std::cout << ">>>>> Greedy法(複数台，ランダム)" << std::endl;
-    solver.SolveGreedyMultiRandom(2);
-    
+    PrintResult(solver.SolveGreedyRandom(2), address);
+
     std::cout << "-------問2-------" << std::endl;
     // 問2
     auto address2 = readAddress("data/address2.txt");
@@ -64,15 +64,15 @@ int main(void) {
     ShortestPathMap pathmap2 = ShortestPathMap();
     pathmap2.AddAdress(address2, d2);
 
-    auto solver2 = Solver(pathmap2, address2, 73);
+    auto solver2 = Solver(pathmap2, address2, 73, 1000);
     std::cout << ">>>>> Greedy法(一度，決め打ち)" << std::endl;
-    PrintResult(solver2.SolveGreedy(), address2);
+    PrintResult(solver2.SolveGreedy(1), address2);
     std::cout << ">>>>> Greedy法(ランダム選択を複数回)" << std::endl;
-    PrintResult(solver2.SolveGreedyRandom(), address2);
+    PrintResult(solver2.SolveGreedyRandom(1), address2);
     std::cout << ">>>>> 焼きなまし法" << std::endl;
     PrintResult(solver2.SA(), address2);
     std::cout << ">>>>> Greedy法(複数台，決め打ち)" << std::endl;
-    solver2.SolveGreedyMulti(2);
+    PrintResult(solver2.SolveGreedy(2), address2);
     std::cout << ">>>>> Greedy法(複数台，ランダム)" << std::endl;
-    solver2.SolveGreedyMultiRandom(2);
+    PrintResult(solver2.SolveGreedyRandom(2), address2);
 }
