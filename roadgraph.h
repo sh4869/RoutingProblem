72de7dc72406c3048310d220798ff96ece0eca95
sh4869 <nobuk4869@gmail.com>
2018-05-16 14:57:13 +0900

    Add MapData type for special way

--- roadgraph.h
+++ roadgraph.h
@@ -3,34 +3,42 @@
 
 #include <array>
 #include <iostream>
+#include <vector>
+#include "util.h"
 
 struct RoadGraph {
-    std::vector<std::vector<int>> w;
+    std::vector<std::vector<double>> w;
     int length;
+    int cost;
     RoadGraph() = default;
-    RoadGraph(const int length,const int start){
+    RoadGraph(const int length, const int _cost, const int defaultspeed) : cost(_cost) {
         w.resize(length * length);
-        for(std::size_t i = 0;i < w.size();i++){
-            w.at(i).resize(length * length) ;
+        for (std::size_t i = 0; i < w.size(); i++) {
+            w.at(i).resize(length * length);
         }
-        int result;
+        double result = 0;
         for (int i = 0; i < length * length; i++) {
             for (int j = 0; j < length * length; j++) {
                 if (i > length && j == i - length)  // 下
-                    result = start;
+                    result = (double)cost / (double)defaultspeed;
                 if (i < length * (length - 1) && j == i + length)  // 上
-                    result = start;
+                    result = (double)cost / (double)defaultspeed;
                 if (i % length != 0 && j == i + 1)  // 右
-                    result = start;
+                    result = (double)cost / (double)defaultspeed;
                 if (i % length != 1 && j == i - 1)  // 左
-                    result = start;
+                    result = (double)cost / (double)defaultspeed;
                 w.at(i).at(j) = result;
                 result = 0;
             }
         }
     }
+    void SetMapData(MapData data) {
+        for (auto d : data) {
+            w.at(d.second.first).at(d.second.second) = (double)cost / d.first;
+        }
+    }
     RoadGraph& operator=(const RoadGraph&) = default;
-    int At(int x, int y) {
+    double At(int x, int y) {
         return w.at(x).at(y);
     }
 };
--- util.h
+++ util.h
@@ -10,7 +10,7 @@
 
 using Address = std::unordered_map<int, std::string>;
 using Result = std::pair<double, std::vector<int>>;
-
+using MapData = std::vector<std::pair<int, std::pair<int, int>>>;
 
 std::vector<std::string> split(const std::string &str, char sep) {
     std::vector<std::string> v;
@@ -33,14 +33,28 @@ Address readAddress(std::string filename) {
     return v;
 }
 
+MapData readMap(std::string filename) {
+    MapData map;
+    std::ifstream ifs(filename);
+    std::string s;
+    while (std::getline(ifs, s)) {
+        auto vec = split(s, ',');
+        auto cost = std::stoi(vec.at(2));
+        map.push_back(
+                std::make_pair(cost, std::make_pair(std::stoi(vec.at(0)), std::stoi(vec.at(1)))));
+        map.push_back(
+                std::make_pair(cost, std::make_pair(std::stoi(vec.at(1)), std::stoi(vec.at(0)))));
+    }
+    return map;
+}
+
 void PrintResult(Result result, Address address) {
     std::cout << address.at(result.second.at(0));
     for (std::size_t i = 1; i < result.second.size(); i++) {
         std::cout << " <- " << address.at(result.second.at(i));
     }
     std::cout << std::endl;
-    std::cout << "cost: " << result.first * 5 / 30 << "h" << std::endl;
+    std::cout << "cost: " << result.first << "h" << std::endl;
 }
-
 
 #endif
